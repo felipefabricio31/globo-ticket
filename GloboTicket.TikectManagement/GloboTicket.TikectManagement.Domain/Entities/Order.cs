5daf42dd560715da5e80d1998dda4c4a0b1f51cf
Felipe Souza <f.souza@expenseon.com>
2022-01-29 15:56:06 -0300

    Add entities class for Domain

---
+++ GloboTicket.TikectManagement/GloboTicket.TikectManagement.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TikectManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public string OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
